01b52734e8ab322f4897d4184967886335c29e38
keith <>
2008-10-10 23:24:17 +0000

    Redesigned atcg.pl which now makes use of a new package (Keith.pm)
    Includes improved and new command-line options as well as better
    error checking (e.g. when sequence does not contain any nucleotides).

--- atcg.pl
+++ atcg.pl
@@ -2,7 +2,7 @@
 #
 # atcg.pl 
 #
-# A script to count GC content plus mono- and dinucleotide frequencies
+# A script to count GC content plus mononucleotide frequencies
 #
 # Last updated by: $Author$
 # Last updated on: $Date$
@@ -11,132 +11,157 @@ use strict;
 use warnings;
 use Getopt::Long;
 use FAlite;
+use Keith;
 
-my $help;    # print help
-my $dinuc;   # calculate dinucleotide frequencies as well as mononucleotide frequencies
-my $com;	 # print main part of output suitable for parsing by a program or excel (computer readable)
+my $help;       # print help
+my $tsv;	    # print main part of output suitable for parsing by a program or excel (tab separated values)
+my $individual; # print out individual statistics for each sequence (if there are individualple)
+my $precision;  # how many decimal places to use for output
 
-GetOptions ("dinuc"   => \$dinuc,
-	    	"help"    => \$help,
-			"com"	  => \$com);
+GetOptions ("help"        => \$help,
+			"tsv"	      => \$tsv,
+			"individual"  => \$individual,
+			"precision=i" => \$precision);
 
 
 if($help){
 	print "atcg.pl - a program to calculate nucleotide and dinucleotide composition\n\n";
-    print "1) atcg.pl can analyse multiple sequences, but will concatenate the results together.\n";
-    print "2) use -dinuc option to also calculate dinucleotide frequencies\n";
-    print "3) GC content is calculated irrespective of Ns in input sequence, Masked GC% is\n";
-    print "   calculated using length of sequence MINUS any N characters\n";
-	print "4) -com mode prints one line output of most of the data, suitablef or pasting into excel\n\n";
+    print "1) By default, script will combine all sequences in input file before calculating nucleotide composition\n";
+	print "2) Use -individual option to get statistics for individual sequences\n";
+    print "3) GC content is calculated both for the entire sequence, and also for a masked copy of the sequence\n";
+    print "   (calculated after excluding any Ns or non-ACGT characters)\n";
+	print "4) -tsv mode prints one line of output per sequence in tab separated values, suitable for pasting into excel\n";
+	print "5) Change precision of output with -precision option (default is 2 d.p.)\n";
     exit(0);
 }
 
 
-my %mono = ('A' => 0,'C' => 0,'G' => 0,'T' => 0,'N' => 0);
+$precision = 2 if (!$precision);
+			
 
-# hashes for dinucleotide frequencies and percentages
-my %di;
-my %di_percent;
+# print help options if no arguments are specified							
+if (!$ARGV[0]){
+	system("$0 -help") && die "Could not run atcg.pl -help\n";
+	exit;
+}
 
-# count sequences in input file
-my $n = 0;
-							
+
+
 ############################################################
 # Read sequence file(s), add all sequence to $seq variable
 ############################################################
 
 open(FILE,"$ARGV[0]") || die "Can't open $ARGV[0], please specify the FASTA sequence file you wish atcg.pl\nto analyse on the command line, e.g. atcg.pl filename.\n\n";
 
-
 my $fasta = new FAlite(\*FILE);
 
+# need to store all sequences combined
+my $combined_sequences;
+
+# keep track of how many sequences there were
+my $count = 0;
+
+# print out header line, if we are using -com option
+print "%A\t%C\t%G\t%T\t%N\t%Other\tLength\t%GC\t%GC (masked)\n" if ($tsv);
+
+
 # loop through each sequence in target file
-while(my $entry = $fasta->nextEntry) {	
-	$n++;
-	my $seq = uc($entry->seq);
-
-	# count mononucleotide frequencies
-	$mono{"A"} += ($seq =~ tr/A/A/); 
-	$mono{"C"} += ($seq =~ tr/C/C/); 
-	$mono{"G"} += ($seq =~ tr/G/G/); 
-	$mono{"T"} += ($seq =~ tr/T/T/); 
-	$mono{"N"} += ($seq =~ tr/N/N/); 
-
-	# do we also want to calculate dinucleotides? Skip if we don't
-	next unless ($dinuc || $com);
+while(my $entry = $fasta->nextEntry) {
 	
-	# to count dinucleotides, loop through sequence, take 2 bp and increment the hash counter
-	foreach my $i (0..length($seq)){
-	    my $tmp = substr($seq,$i,2);		
-		$di{$tmp}++;
+	$count++;
+		
+	# are we calculating composition for individual sequences?
+	if($individual){	
+		# grab sequence counts and print output
+		print_output(Keith::get_mononucleotides($entry->seq),$entry->def);
 	}
+
+	# add sequence to running total
+	$combined_sequences .= ($entry->seq);
+	
 }
 close(FILE);
 
-# Calculate final statistics
-# need to determine number of Ns to work out masked length and GC content of masked sequence
-my $total_length = $mono{'A'} + $mono{'C'} + $mono{'G'} + $mono{'T'} + $mono{'N'};
-my $masked_length     = $total_length - $mono{'N'};
-my $total_GC_percent  = sprintf("%3.2f",(($mono{'G'}+$mono{'C'})/$total_length)*100);
-my $masked_GC_percent = sprintf("%3.2f",(($mono{'G'}+$mono{'C'})/$masked_length)*100);
-my $A_percent         = ($mono{'A'}/$total_length)*100;
-my $T_percent         = ($mono{'T'}/$total_length)*100;
-my $C_percent         = ($mono{'C'}/$total_length)*100;
-my $G_percent         = ($mono{'G'}/$total_length)*100;
-my $N_percent         = ($mono{'N'}/$total_length)*100;
 
-# print human or computer readable output?
-unless ($com){
-	print "\nProcessed $n sequences\n\n";
-	print "Length\tGC%\tMasked length\tMasked GC%:\n";
-	print "$total_length\t$total_GC_percent\t$masked_length\t$masked_GC_percent\n\n";
 
-	print "A\tC\tG\tT\tN\n";
-	print "$mono{'A'}\t$mono{'C'}\t$mono{'G'}\t$mono{'T'}\t$mono{'N'}\n\n";
-
-	print "A%\tC%\tG%\tT%\tN%\n";
-	printf "%3.2f\t%3.2f\t%3.2f\t%3.2f\t%3.2f\n\n", $A_percent, $C_percent, $G_percent, $T_percent, $N_percent;
-}
-
-exit(0) unless ($dinuc || $com);
+# can now print output for combined sequence, unless using -tsv & -individual options
+print_output(Keith::get_mononucleotides($combined_sequences),"All sequences combined") unless ($individual && $tsv);
 
 
 
-######################################
-# calculate dinucleotide percentages
-######################################
+################################################
+#
+# One main subroutine to handle printing output
+#
+################################################
 
-				
+sub print_output{
+	my ($a,$c,$g,$t,$n,$o,$header) = @_;
 
-my $dinuc_total = $di{'AA'}+$di{'AC'}+$di{'AG'}+$di{'AT'} + 
-				  $di{'CA'}+$di{'CC'}+$di{'CG'}+$di{'CT'} +
-				  $di{'GA'}+$di{'GC'}+$di{'GG'}+$di{'GT'} +
-				  $di{'TA'}+$di{'TC'}+$di{'TG'}+$di{'TT'};
+	# first calculate length
+	my $total_length = $a + $c + $g + $t + $n + $o;
 
-# will make four lines of output text
-my ($out1,$out2,$out3,$out4);				
-foreach my $i qw(AA AC AG AT CA CC CG CT GA GC GG GT TA TC TG TT){
+	# print out header if we are individual mode, else print total sequence count
+	# but don't print this out with -com option selected
+	if($individual && !$tsv){
+		print '-' x 60, "\n";
+		print "$header\n";
+		print '-' x 60, "\n";
+	}
+	elsif(!$tsv){
+		print "\nProcessed $count sequences\n\n";
+	}
 	
-	# set to 0 if no values exist for that dinucleotide
-	($di{$i} = 0) if (!defined($di{$i}));
+	# if there was no sequence (for some reason), we should print an error and stop
+	if ($total_length == 0){
+
+		# if using TSV output, just print zeroes as there is no sequence
+		if($tsv){
+			print "0\t" x 9,  "\n";
+		}
+		else{
+			print "WARNING: no sequence detected\n\n\n\n";
+
+		}
+		return;
+	}
 	
-	$di_percent{$i} = (($di{$i}/$dinuc_total)*100);
-	$out1 .= "$i\t";
-	$out2 .= "$di{$i}\t";
-	$out3 .= "$i%\t";
-	$out4 .= sprintf("%3.2f\t",$di_percent{$i});
+
+	# Calculate final statistics
+	my $gc_percent  = sprintf("%3.${precision}f",(($g + $c)/$total_length)*100);
+
+	# masked length ignores Ns (or other non-AGCT characters) in sequence
+	my $masked_length     = $a + $c + $g + $t;
+	my $masked_gc_percent = sprintf("%3.${precision}f",(($g + $c)/$masked_length)*100);
+
+	my $a_percent         = sprintf("%3.${precision}f",($a / $total_length)*100);
+	my $c_percent         = sprintf("%3.${precision}f",($c / $total_length)*100);
+	my $g_percent         = sprintf("%3.${precision}f",($g / $total_length)*100);
+	my $t_percent         = sprintf("%3.${precision}f",($t / $total_length)*100);
+	my $n_percent         = sprintf("%3.${precision}f",($n / $total_length)*100);
+	my $o_percent         = sprintf("%3.${precision}f",($o / $total_length)*100);
+	
+	# print concise output in TSV format, or more verbose details?
+	if($tsv){
+		print "$a_percent\t$c_percent\t$g_percent\t$t_percent\t$n_percent\t$o_percent\t$total_length\t$gc_percent\t$masked_gc_percent\n";
+	}
+	else{			
+		print "Length\tGC%\n";
+		print "$total_length\t$gc_percent\n\n";
+
+		print "Masked length\tMasked GC%\n";
+		print "$masked_length\t$masked_gc_percent\n\n";
+
+		print "A\tC\tG\tT\tN\tOther\n";
+		print "$a\t$c\t$g\t$t\t$n\t$o\n\n";
+
+		print "A%\tC%\tG%\tT%\tN%\tOther%\n";
+		printf "%3.${precision}f\t%3.${precision}f\t%3.${precision}f\t%3.${precision}f\t%3.${precision}f\t%3.${precision}f\n\n\n\n", 
+		$a_percent, $c_percent, $g_percent, $t_percent, $n_percent, $o_percent;
+		
+	}
 }
 
-if(!$com){
-	print "$out1\n$out2\n\n$out3\n$out4\n\n";
-}
-else{
-	print "\nN\tLength\tGC%\tA%\tC%\tG%\tT%\tN%\t$out3\n";
-	print "$n\t$total_length\t$total_GC_percent\t";
-	printf "%3.2f\t%3.2f\t%3.2f\t%3.2f\t%3.2f\t", $A_percent, $C_percent, $G_percent, $T_percent, $N_percent;
-	print "$out4\n\n";
-
-}
 
 exit(0);
 
