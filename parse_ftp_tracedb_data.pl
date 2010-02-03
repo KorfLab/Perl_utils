46edb8286bd7f657db5c7847567e52a1a6df0dbc
keith <>
2010-02-03 23:53:50 +0000

    Fixed bug with generating fasta file names

--- parse_ftp_tracedb_data.pl
+++ parse_ftp_tracedb_data.pl
@@ -173,9 +173,8 @@ FILE: foreach my $anc_file (@anc_files) {
 		
 	# make versions for fasta file name
 	my ($fasta_file, $fasta_file_name) = ($anc_file, $anc_file_name);
-	$fasta_file =~ s/^anc\./fasta\./;
+	$fasta_file =~ s/\/anc\./\/fasta\./;
 	$fasta_file_name =~ s/^anc\./fasta\./;
-
 	
 	# no point proceeding if we can't find the accompanying FASTA file in the same directory
 	if (! -e $fasta_file){
